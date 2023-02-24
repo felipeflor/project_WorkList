8722129e7f558a5122ed9af171e69322ce3ca3fe
Felipe <felipedossantosflor@gmail.com>
2023-02-24 13:55:03 -0300

    @FelipeFlor [Task: Add swagger]

--- WorkListAPI/Startup.cs
+++ WorkListAPI/Startup.cs
@@ -18,6 +18,9 @@ using WorkListAPI.Src.Services.Implements;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.IO;
 
 namespace WorkListAPI
 {
@@ -68,36 +71,84 @@ namespace WorkListAPI
                 };
             }
             );
+
+            //Swagger configuration
+            services.AddSwaggerGen(
+            s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Work List",
+                    Version = "v1"
+                });
+
+                s.AddSecurityDefinition(
+                    "Bearer",
+                    new OpenApiSecurityScheme()
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Description = "JWT authorization header uses: Bearer + JWT Token",
+                    });
+
+                s.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new List<string>()
+                        }
+                    });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                s.IncludeXmlComments(xmlPath);
+            });
         }
 
-    }
-    
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Context context)
-    {
-        //Development environment
-        if (env.IsDevelopment())
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Context context)
         {
+            //Development environment
+            if (env.IsDevelopment())
+            {
+                context.Database.EnsureCreated();
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Work List v1");
+                    c.RoutePrefix = string.Empty;
+                });
+
+            }
+
+            //Prodution environment
             context.Database.EnsureCreated();
-            app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseCors(c => c
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
-
-        //Prodution environment
-        context.Database.EnsureCreated();
-
-        app.UseRouting();
-
-        app.UseCors(c => c
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader());
-
-        app.UseAuthorization();
-        app.UseAuthentication();
-
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
     }
 }
